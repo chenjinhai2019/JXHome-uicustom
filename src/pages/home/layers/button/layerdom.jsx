0af14c7004db4451e6ce667fd501bec45ecc75e7
chenjinhai2019 <2895344291@qq.com>
2019-01-18 09:46:49 +0800

    button组件新增文字颜色选择器

--- src/pages/home/layers/button/layerdom.jsx
+++ src/pages/home/layers/button/layerdom.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 // 设置 dom
 export function layerdom(layer, zIndex) {
-
-    return <button className="submit-inner" data-type="submit" data-submittype="all" dangerouslySetInnerHTML={{ __html: layer.data }} />;
+    console.log("button 设置",layer.estyle);
+    return <button className="submit-inner" data-type="submit" data-submittype="all"
+                   dangerouslySetInnerHTML={{ __html: layer.data.buttonValue }} />;
 }
